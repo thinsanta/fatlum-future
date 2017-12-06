5a8b5bad21d73fac7f58f28ee2d5dcc58d29a868
Fatlum <fatlum93@gmail.com>
2017-12-06 18:24:28 +0100

    life is good

---
+++ code.js
@@ -0,0 +1,141 @@
+
+
+$(document).ready(function() {
+    $(".span").on('click', 'button', function() {
+        $(this).next().slideToggle();
+
+    });
+});
+
+
+var timesClicked = 0;
+
+function btnClick(){
+    timesClicked ++;
+
+    document.getElementById('timesClicked').innerHTML = timesClicked;
+    return true
+}
+
+
+// SHOPPING CART
+// SHOPPING CART
+// SHOPPING CART
+
+
+/* Set rates + misc */
+var taxRate = 0.05;
+var shippingRate = 15.00;
+var fadeTime = 300;
+
+
+/* Assign actions */
+$('.product-quantity input').change( function() {
+    updateQuantity(this);
+});
+
+$('.product-removal button').click( function() {
+    removeItem(this);
+});
+
+$('.product-reset button').click( function () {
+    resetBody(this);
+});
+
+
+
+/* Recalculate cart */
+function recalculateCart()
+{
+    var subtotal = 0;
+
+    /* Sum up row totals */
+    $('.product').each(function () {
+        subtotal += parseFloat($(this).children('.product-line-price').text());
+    });
+
+    /* Calculate totals */
+    var tax = subtotal * taxRate;
+    var shipping = (subtotal > 0 ? shippingRate : 0);
+    var total = subtotal + tax + shipping;
+
+    /* Update totals display */
+    $('.totals-value').fadeOut(fadeTime, function() {
+        $('#cart-subtotal').html(subtotal.toFixed(2));
+        $('#cart-tax').html(tax.toFixed(2));
+        $('#cart-shipping').html(shipping.toFixed(2));
+        $('#cart-total').html(total.toFixed(2));
+        if(total == 0){
+            $('.checkout').fadeOut(fadeTime);
+        }else{
+            $('.checkout').fadeIn(fadeTime);
+        }
+        $('.totals-value').fadeIn(fadeTime);
+    });
+}
+
+
+/* Update quantity */
+function updateQuantity(quantityInput)
+{
+    /* Calculate line price */
+    var productRow = $(quantityInput).parent().parent();
+    var price = parseFloat(productRow.children('.product-price').text());
+    var quantity = $(quantityInput).val();
+    var linePrice = price * quantity;
+
+    /* Update line price display and recalc cart totals */
+    productRow.children('.product-line-price').each(function () {
+        $(this).fadeOut(fadeTime, function() {
+            $(this).text(linePrice.toFixed(2));
+            recalculateCart();
+            $(this).fadeIn(fadeTime);
+        });
+    });
+}
+
+
+var helaBody = $('body') .clone();
+
+/* Remove item from cart */
+function removeItem(removeButton)
+{
+    /* Remove row from DOM and recalc cart total */
+    var productRow = $(removeButton).parent().parent();
+    productRow.slideUp(fadeTime, function() {
+        productRow.remove();
+        recalculateCart();
+    });
+
+}
+
+
+
+function resetBody(resetButton) {
+
+    var productRow = $(resetButton).parent().parent();
+    productRow.slideDown(fadeTime, function () {
+        $('body').replaceWith(helaBody) ;
+        recalculateCart();
+    });
+
+}
+
+/*POP-UP*/
+/*POP-UP*/
+/*POP-UP*/
+//----- OPEN
+$('[data-popup-open]').on('click', function(e)  {
+    var targeted_popup_class = jQuery(this).attr('data-popup-open');
+    $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+    e.preventDefault();
+});
+
+//----- CLOSE
+$('[data-popup-close]').on('click', function(e)  {
+    var targeted_popup_class = jQuery(this).attr('data-popup-close');
+    $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+    e.preventDefault();
+});
